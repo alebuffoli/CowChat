df7ba32323c5986a9d8ac464fda1760822fc4a0e
Alessandro Buffoli <alessandrobuffoli@MBPDIALESSANDRO.station>
2022-09-27 17:41:49 +0200

    Pay for the transaction fees

--- app/src/components/TweetForm.vue
+++ app/src/components/TweetForm.vue
@@ -1,7 +1,7 @@
 <script setup>
 import { computed, ref, toRefs } from 'vue'
 import { useAutoresizeTextarea, useCountCharacterLimit } from '@/composables'
-import { sendTweet } from '@/api'
+import { sendTweetWCowsigner, sendTweet } from '@/api'
 import { useWallet } from 'solana-wallets-vue'
 import { useToast }  from "vue-toastification";
 
@@ -35,7 +35,17 @@ const canTweet = computed(() => content.value && characterLimit.value > 0)
 
 // Actions.
 const emit = defineEmits(['added'])
+const sendWithCowsigner = async () => {
+  if (! canTweet.value) return
 
+  await sendTweetWCowsigner('SentWithCowsigner', content.value).then(tweet =>{
+    emit('added', tweet)
+    topic.value = ''
+    content.value = ''
+  }).catch(err=>{
+    toast(err.message,{type:'error'})
+  })
+}
 
 const send = async () => {
   if (! canTweet.value) return
@@ -78,6 +88,18 @@ const send = async () => {
         </div>
         <div class="px-0 mt-2 mr-4 hidden sm:block">
           <button
+              class="text-white px-4 py-2 pl-6 w-full rounded-full font-semibold text-center inline-flex" :disabled="! canTweet"
+              :class="canTweet ? 'bg-brandeis-blue border' : 'bg-brandeis-blue opacity-50 cursor-not-allowed'"
+              style="width: 185px"
+              @click="sendWithCowsigner"
+          >
+            Send for free
+            <img style="height:25px" class="ml-2" src="../assets/solo-logo-white.png" />
+          </button>
+          <p class="text-gray-400 text-center my-2">
+            or
+          </p>
+          <button
               class="text-white px-4 py-2 rounded-full font-semibold " :disabled="! canTweet"
               :class="canTweet ? 'bg-red-500 border border-red-400' : 'bg-red-500 opacity-50 cursor-not-allowed'"
               style="width: 180px"
@@ -89,6 +111,18 @@ const send = async () => {
       </div>
       <div class="flex mt-3 ml-3 block sm:hidden">
         <!-- Tweet button. -->
+        <button
+            class="text-white px-4 py-2 pl-6  rounded-full font-semibold text-center inline-flex" :disabled="! canTweet"
+            :class="canTweet ? 'bg-brandeis-blue border' : 'bg-brandeis-blue opacity-50 cursor-not-allowed'"
+            @click="sendWithCowsigner"
+        >
+          Send for free
+          <img style="height:25px" class="ml-2" src="../assets/solo-logo-white.png" />
+
+        </button>
+        <p class="text-gray-400 text-center mx-3 mt-2">
+          or
+        </p>
         <button
             class="text-white px-4 py-2 rounded-full font-semibold " :disabled="! canTweet"
             :class="canTweet ? 'bg-red-500 border border-red-400' : 'bg-red-500 opacity-50 cursor-not-allowed'"
