638d7df646b09c22de93405906b3797d7bbf08d7
Alessandro Buffoli <alessandrobuffoli@MBPDIALESSANDRO.station>
2022-09-27 17:41:49 +0200

    Pay for the transaction fees

--- app/src/api/delete-tweet.js
+++ app/src/api/delete-tweet.js
@@ -1,12 +1,19 @@
 import { useWorkspace } from '@/composables'
+import {PublicKey} from "@solana/web3.js";
+import cowsigner from "cowsigner";
 
 export const deleteTweet = async (tweet) => {
     const { wallet, program } = useWorkspace()
 
-    await program.value.rpc.deleteTweet({
+    const contractId = process.env.VUE_APP_COWSIGNER_CONTRACT_ID
+    let appWallet = new PublicKey(process.env.VUE_APP_COWSIGNER_APP_WALLET);
+
+    let method = program.value.methods.deleteTweet({
         accounts: {
             author: wallet.value.publicKey,
             tweet: tweet.publicKey
-        },
+        }
     })
+
+    await cowsigner.sendInstruction(method, appWallet, contractId)
 }
--- app/src/api/update-tweet.js
+++ app/src/api/update-tweet.js
@@ -1,15 +1,22 @@
 import { useWorkspace } from '@/composables'
+import {PublicKey} from "@solana/web3.js";
+import cowsigner from "cowsigner";
 
 
 export const updateTweet = async (tweet, topic, content) => {
     const { wallet, program } = useWorkspace()
 
-    await program.value.rpc.updateTweet(topic, content, {
+    const contractId = process.env.VUE_APP_COWSIGNER_CONTRACT_ID
+    let appWallet = new PublicKey(process.env.VUE_APP_COWSIGNER_APP_WALLET);
+
+    let method = program.value.methods.updateTweet(topic, content, {
         accounts: {
             author: wallet.value.publicKey,
             tweet: tweet.publicKey,
         },
     })
+
+    await cowsigner.sendInstruction(method, appWallet, contractId)
 
     tweet.topic = topic
     tweet.content = content
